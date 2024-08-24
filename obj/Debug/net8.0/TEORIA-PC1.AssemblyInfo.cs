95a97638a37eb64247137fcf9ef98874f45da8c5
tatiana17899 <tatianasuarezrosas332@gmail.com>
2024-08-24 16:34:00 -0500

    index

--- obj/Debug/net8.0/TEORIA-PC1.AssemblyInfo.cs
+++ obj/Debug/net8.0/TEORIA-PC1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TEORIA-PC1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+794854ae162a427cdf96dc5c127294483856b1be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d478a7b02b29d078c834efd595917e012369bff")]
 [assembly: System.Reflection.AssemblyProductAttribute("TEORIA-PC1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TEORIA-PC1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
